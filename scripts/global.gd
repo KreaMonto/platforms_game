4219a90e350cdeea8d25cce6b0b93c0855bdb3d5
Krea Monto <sanrojasbusiness@gmail.com>
2025-05-01 22:01:15 -0500

    initial

---
+++ scripts/global.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+var coins = 0
+
+var axis : Vector2
+
+func get_movement() -> Vector2:
+	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	axis.y = int(Input.is_action_pressed("ui_up")) - int(Input.is_action_just_pressed("ui_down"))
+	return axis.normalized()
---
+++ scripts/player_slime.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -400.0
+
+@onready var sprite = $Sprite2D
+@onready var animationLayer = $AnimationPlayer
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	
+	var friction = false
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		if friction == true:
+			velocity.x = lerp(velocity.x, 0, 0.5)
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		animationLayer.play("jump")
+
+	# handle direction
+	velocity.x = GLOBAL.get_movement().x * SPEED
+	if velocity.x > 0:
+		animationLayer.play("walk")
+		sprite.flip_h = true
+	elif velocity.x < 0:
+		animationLayer.play("walk")
+		sprite.flip_h = false
+	else:
+		animationLayer.play("idle")
+		friction = true
+
+	move_and_slide()
